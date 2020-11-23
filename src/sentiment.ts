c0969dd12f7c3d3c4c6c638a365e43a655c5cae7
Alexis GADONNEIX <alexis.gadonneix@eleves.enpc.fr>
2020-11-23 19:21:35 +0100

    ID et Key dans le code

--- src/sentiment.ts
+++ src/sentiment.ts
@@ -1,10 +1,16 @@
 import AWS from "aws-sdk";
 
+AWS.config.update({
+  apiVersion: "2017-11-27",
+  accessKeyId: "AKIAS2KGGOLMF6VP7YDF",
+  secretAccessKey: "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",
+  region: "eu-west-2"
+})
 
-const comprehend = new AWS.Comprehend({apiVersion: '2017-11-27',region:"eu-west-2"});
+const comprehend = new AWS.Comprehend();
 const params = {
     LanguageCode: "fr", /* required */
-    Text: 'rxcfygvhbjncgvhb' /* required */
+    Text: 'sentiments mitigés' /* required */
   };
 comprehend.detectSentiment(params, (err, data) => {
     if (err) console.log(err, err.stack); // an error occurred
