b9ea2fa0487ba1ca5250f69f4782c4c838875b9b
RémiSimon <remi.simon@eleves.enpc.fr>
2020-11-24 19:47:25 +0100

    réutilisation de detectSentiment

--- src/sentiment.ts
+++ src/sentiment.ts
@@ -18,11 +18,9 @@ const comprehend = new AWS.Comprehend();
 const rawData = readFileSync(join(__dirname, "data-anonymized.json"));
 const rawMessages: Message[] = JSON.parse(rawData.toString());
 
-const completeData = cloneDeep(rawMessages);
-
-type SentimentRequestType = {
+type SentimentType = {
     LanguageCode: string /* required */;
-    TextList: Array<string> /* required */;
+    Text: string /* required */;
 };
 
 // const params: SentimentRequestType = {
@@ -30,35 +28,36 @@ type SentimentRequestType = {
 //     TextList: rawMessages.map(m => m.content)
 // };
 
-const detectSentiment = async (
-    params: SentimentRequestType
-): Promise<Message[]> => {
+const detectSentiment = async (params: SentimentType) => {
     return new Promise((resolve, reject) => {
-        comprehend.batchDetectSentiment(params, (err, data) => {
+        comprehend.detectSentiment(params, (err, data) => {
             if (err) {
                 reject(err);
             } else {
-                for (let i = 0; i < rawMessages.length; i++) {
-                    completeData[i]["sentiment"] = data["ResultList"][i];
-                }
-
-                resolve(completeData);
+                resolve(data);
             }
         });
     });
 };
 
-const main = async () => {
-    const sentimentMessages = await detectSentiment({
-        LanguageCode: "fr",
-        TextList: rawMessages.map(m => m.content)
-    });
+const completeData = [];
 
-    console.log(sentimentMessages);
+const main = async () => {
+    for (const message of rawMessages) {
+        let messageWithSentiment = cloneDeep(message);
+        const sentimentMessage = await detectSentiment({
+            LanguageCode: "fr",
+            Text: message.content
+        });
+        messageWithSentiment["sentiment"] = sentimentMessage;
+        completeData.push(messageWithSentiment);
+    }
+
+    console.log(completeData);
 
     writeFileSync(
         join(__dirname, "data-with-sentiment.json"),
-        JSON.stringify(sentimentMessages)
+        JSON.stringify(completeData)
     );
 };
 
